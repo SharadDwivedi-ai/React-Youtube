489c48ea55acb36a284ab26dbdce6cd23f42cded
Sharad <srd28nov21@gmail.com>
2026-02-25 23:33:24 +0530

    Setup React

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (  
+    <div>
+      <h1>Sharad & React with Vite </h1>
+      <p>Today i am learning for setup the vite third party library for create the React Application</p>
+    </div>
+  )
+}
+
+export default App;
