7fbbbc21017668721d1f7befa969fb804003a74e
Sharad Dwivedi <srd28nov21@gmail.com>
2026-04-01 20:28:35 +0000

    Change Heading and Paragraph

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
 
-  return (  
+  return (
     <div>
       <h1>Sharad & React with Vite </h1>
       <p>Today i am learning for setup the vite third party library for create the React Application</p>
